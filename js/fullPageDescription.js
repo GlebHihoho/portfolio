b10329b074c37802d78161e8d4325de4fe79806d
Gleb Motuz <hihoho@mail.ru>
2016-11-01 21:46:54 +0300

    Настроил fullpage плагин

---
+++ js/fullPageDescription.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+      sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+      anchors: ['firstPage', 'secondPage', 'thirdPage'],
+
+      menu: '#menu',
+      navigation: true
+    });
+});
